dd635748e15b108635b6c509debbd85a9496cd3c
Matt Davis <mattdavis9@gmail.com>
2018-05-14 21:21:36 -0700

    IFD Parsing

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
+#define _BSD_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+#include <endian.h>
 #include <errno.h>
 #include <string.h>
 #include <stdbool.h>
 
+/* Great resources:
+ * TIFF (Format of Exif data):
+ * https://www.adobe.io/content/udp/en/open/standards/TIFF/_jcr_content/contentbody/download/file.res/TIFF6.pdf
+ *
+ * Exif/JPEG:
+ * https://www.media.mit.edu/pia/Research/deepview/exif.html
+ *
+ * My other program: TEPSniff (another TIFF parser)
+ * the endian code came from that project.
+ * https://github.com/enferex/tepsniff/
+ */
+
 #define MARKER_SOI 0xD8 // Start of Image
 #define MARKER_EOI 0xD9 // End of Image
 #define MARKER_SOS 0xDA // Start of Stream
-
 
 #define _PR(_fd, _pre, _msg, ...)        \
   do {                                   \
@@ -30,7 +43,63 @@
 #define DBG(...)
 #endif
 
+/* Macros to handle endiness of TIFF. 4 cases.
+ * This was originally from my other project:
+ * https://github.com/enferex/tepsniff/
+ * 1) BE system and TIFF is BE
+ * 2) BE system and TIFF is LE
+ * 3) LE system and TIFF is BE
+ * 4) LE system and TIFF is LE
+ */
+#define IS_BE(_h) (_h->hdr.byte_order == 0x4D4D)
+#if __BYTE_ORDER == __BIG_ENDIAN
+#define NATIVE2(_h,_v) (IS_BE(_h) ? (_v) : le16toh(_v))
+#define NATIVE4(_h,_v) (IS_BE(_h) ? (_v) : le32toh(_v))
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
+#define NATIVE2(_h,_v) (IS_BE(_h) ? htobe16(_v) : (_v))
+#define NATIVE4(_h,_v) (IS_BE(_h) ? htobe32(_v) : (_v))
+#else
+#error "Middle endian not supported."
+#endif
+
 typedef struct {uint32_t lat, lon; } coords_t;
+
+typedef struct _ifd_entry_t {
+  uint16_t tag;
+  uint16_t type;
+  uint32_t n_components;
+  uint32_t value_offset;
+} ifd_entry_t;
+
+typedef struct _ifd_hdr_t {
+  uint16_t n_entries;
+  ifd_entry_t entries[0];
+} ifd_hdr_t;
+
+// This is really the entire IFD but with a next pointer so that we can chain
+// IFDs together.
+typedef struct _ifd_t {
+  ifd_hdr_t hdr;
+  struct _ifd_t *next;
+} ifd_t;
+
+typedef struct _tiff_hdr_t {
+  uint16_t byte_order; // "II": Little endian, "MM": Big endian.
+  uint16_t universe;   // Must be 42.
+  uint32_t offset;     // Initial IFD offset
+} tiff_hdr_t;
+
+typedef struct _tiff_t {
+  tiff_hdr_t hdr;
+  ifd_t *ifds;
+} tiff_t;
+
+typedef struct _exif_t {
+#define EXIF_HEADER_BYTES 6 // This are the first 6 bytes in 'data': 'Exif00'
+  size_t   size;
+  uint8_t *data;
+  tiff_t  *tiff;
+} exif_t;
 
 static void usage(const char *execname) {
   printf("Usage: %s [jpeg ...]\n", execname);
@@ -68,20 +137,6 @@ static uint16_t read_marker_size(FILE *fp) {
   return (hi << 8) | lo;
 }
 
-// Brute-force scan to look for the value 'marker' in the file stream.
-#if 0
-static _Bool seek_to_next(FILE *fp, uint8_t marker) {
-  while (!feof(fp) && !ferror(fp)) {
-    const uint8_t byte = safe_fgetc(fp);
-    if (byte == 0xFF && (safe_fgetc(fp) == marker)) {
-      fseek(fp, -2, SEEK_CUR);
-      return true;
-    }
-  }
-  return false;
-}
-#endif // if 0
-
 static _Bool find_marker(FILE *fp, int marker) {
   while (!feof(fp) && !ferror(fp)) {
     const uint8_t current_marker = read_marker(fp, true);
@@ -92,7 +147,6 @@ static _Bool find_marker(FILE *fp, int marker) {
     // Special cases.
     if (current_marker == MARKER_SOS)
       return false; // This will lead to the EOI, so we are done with this image.
-      //return seek_to_next(fp, MARKER_EOI);
     else if (current_marker == MARKER_SOI || current_marker == MARKER_EOI)
       continue;
     else {
@@ -107,15 +161,10 @@ static _Bool find_marker(FILE *fp, int marker) {
   return false;
 }
 
-typedef struct _exif_t {
-  size_t   size;
-  uint8_t *data;
-} exif_t;
-
 static exif_t *read_exif(const char *filename) {
   DBG("Loading %s", filename);
   FILE *fp = fopen(filename, "r");
-  if (!fp) 
+  if (!fp)
     FAIL("Error opening %s: %s", filename, strerror(errno));
 
   exif_t *ex;
@@ -126,7 +175,7 @@ static exif_t *read_exif(const char *filename) {
   const uint8_t header_marker = read_marker(fp, false);
   if (header_marker != 0xD8) {
     DBG("%s: Not a file format we know how to handle.", filename);
-    goto oops; // Not an image format we know how to handle. 
+    goto oops; // Not an image format we know how to handle.
   }
 
   // Find the start of APP1 (where exif lives).
@@ -156,8 +205,87 @@ oops:
     return NULL;
 }
 
-static _Bool read_exif(const exif_t *ex) {
-  return true;
+static void free_exif(exif_t *ex) {
+  free(ex->data);
+  free(ex);
+}
+
+// Given an exif and an offset, read data from the exif data blob.
+static _Bool read_data_from_exif(
+    void         *dest,
+    uint64_t      offset,
+    size_t        size,
+    const exif_t *ex) {
+  if (offset + size > ex->size)
+    return false;
+  return memcpy(dest, ex->data + offset, size) != NULL;
+}
+
+// This updates offset to point at the end of just-read-in ifd.
+static ifd_t *read_ifd(const exif_t *ex, const tiff_t *tiff, uint64_t *offset) {
+  // Read in the number of entries for this IFD.
+  uint16_t n_entries;
+  if (!read_data_from_exif((void *)&n_entries, *offset, sizeof(n_entries), ex))
+    FAIL("Error loading IFD entry count.");
+  n_entries = NATIVE2(tiff, n_entries);
+  *offset += sizeof(n_entries);
+
+  const size_t n_bytes = n_entries * sizeof(ifd_entry_t);
+  ifd_t *ifd = calloc(1, n_bytes);
+  if (!ifd)
+    FAIL("Error allocating memory to store an IFD.");
+
+  // Advance to the start of the first entry.
+  ifd->hdr.n_entries = n_entries;
+  if (!read_data_from_exif((void *)ifd->hdr.entries, *offset, n_bytes, ex))
+    FAIL("Error loading IFD entry count.");
+  *offset += n_bytes;
+
+  // Put the values for each entry into native parlance.
+  for (int i=0; i<n_entries; ++i) {
+    ifd_entry_t *entry = &ifd->hdr.entries[i];
+    entry->tag = NATIVE2(tiff, entry->tag);
+    entry->type = NATIVE2(tiff, entry->type);
+    entry->n_components = NATIVE4(tiff, entry->n_components);
+    entry->value_offset = NATIVE4(tiff, entry->value_offset);
+  }
+
+  return ifd;
+}
+
+static tiff_t *exif_to_tiff(const exif_t *ex) {
+  tiff_t *tiff = calloc(1, sizeof(tiff_t));
+  if (!tiff)
+    FAIL("Error allocating memory to store Exif in TIFF format.");
+
+  if (!read_data_from_exif((void *)tiff,
+       EXIF_HEADER_BYTES, sizeof(tiff->hdr), ex))
+    FAIL("Error reading TIFF header.");
+
+  // Fix up the header so that we can make sense of it.
+  tiff->hdr.universe = NATIVE2(tiff, tiff->hdr.universe);
+  tiff->hdr.offset   = NATIVE4(tiff, tiff->hdr.offset);
+
+  // Sanity check.
+  if (tiff->hdr.universe != 42)
+    FAIL("Invalid TIFF format.");
+
+  // Initialize the IFD scan by placing the offset at the beginning.
+  uint64_t off = EXIF_HEADER_BYTES + tiff->hdr.offset;
+
+  // Read in all of the IFD entries.
+  ifd_t *prev = NULL;
+  while (off < ex->size) {
+    ifd_t *ifd = read_ifd(ex, tiff, &off);
+    ifd->next = prev;
+    prev = ifd;
+  }
+
+  return tiff;
+}
+
+static void free_tiff(tiff_t *tiff) {
+  free(tiff);
 }
 
 int main(int argc, char **argv) {
@@ -170,11 +298,12 @@ int main(int argc, char **argv) {
 
   // For each file specified on the command line.
   for (int i=1; i<argc; ++i) {
-    exif_t *ex = read_exif(argv[i]);
-    parse_exif(ex);
-    if (!ex)
-      continue;
-    free(ex);
+    exif_t *ex;
+    if ((ex = read_exif(argv[i]))) {
+      tiff_t *tiff = exif_to_tiff(ex);
+      free_exif(ex);
+      free_tiff(tiff);
+    }
   }
 
   return 0;
